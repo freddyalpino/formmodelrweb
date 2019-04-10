ae837e7ce9c84ce35beb4082f4020a829de7ded7
freddyalpino <35858296+freddyalpino@users.noreply.github.com>
2019-04-10 23:54:21 +0700

    Add files via upload

---
+++ db_loginrweb2019.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2019 at 05:33 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_loginrweb2019`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_matkul`
+--
+
+CREATE TABLE `tbl_matkul` (
+  `id_matkul` int(11) NOT NULL,
+  `nama_matkul` varchar(150) NOT NULL,
+  `semester` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_matkul`
+--
+
+INSERT INTO `tbl_matkul` (`id_matkul`, `nama_matkul`, `semester`) VALUES
+(1, 'applku', 9),
+(2, 'prpl', 4),
+(3, 'rweb', 6),
+(4, 'alpro', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `username`, `email`, `password`) VALUES
+(1, 'gema antika hariadi', 'gemaantikahr@gmail.com', 'gemaantikahr'),
+(2, 'prabowo jokowi', 'prabowojokowi@gmail.com', 'pemilu2019');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_matkul`
+--
+ALTER TABLE `tbl_matkul`
+  ADD PRIMARY KEY (`id_matkul`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_matkul`
+--
+ALTER TABLE `tbl_matkul`
+  MODIFY `id_matkul` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
